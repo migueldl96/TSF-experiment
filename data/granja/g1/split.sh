f4e4c055d711abbf5515b68d4c104c8ce210166e
Miguel Díaz Lozano <migueldialoz@gmail.com>
2018-06-07 21:06:25 +0200

    Añadidos ficheros de datos.

---
+++ data/granja/g1/split.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+cut -f1 test.csv > test/windSpeed.txt
+cut -f2 test.csv > test/windDir.txt
+cut -f3 test.csv > test/rea1.txt
+cut -f4 test.csv > test/rea2.txt
+cut -f5 test.csv > test/rea3.txt
+cut -f6 test.csv > test/rea4.txt
+cut -f7 test.csv > test/rea5.txt
+cut -f8 test.csv > test/rea6.txt
+cut -f9 test.csv > test/rea7.txt
+cut -f10 test.csv > test/rea8.txt
+cut -f11 test.csv > test/rea9.txt
+cut -f12 test.csv > test/rea10.txt
+cut -f13 test.csv > test/rea11.txt
+cut -f14 test.csv > test/rea12.txt
+cut -f15 test.csv > test/ramp.txt
+
+cut -f1 train.csv > train/windSpeed.txt
+cut -f2 train.csv > train/windDir.txt
+cut -f3 train.csv > train/rea1.txt
+cut -f4 train.csv > train/rea2.txt
+cut -f5 train.csv > train/rea3.txt
+cut -f6 train.csv > train/rea4.txt
+cut -f7 train.csv > train/rea5.txt
+cut -f8 train.csv > train/rea6.txt
+cut -f9 train.csv > train/rea7.txt
+cut -f10 train.csv > train/rea8.txt
+cut -f11 train.csv > train/rea9.txt
+cut -f12 train.csv > train/rea10.txt
+cut -f13 train.csv > train/rea11.txt
+cut -f14 train.csv > train/rea12.txt
+cut -f15 train.csv > train/ramp.txt
---
+++ data/granja/g3/split.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+cut -d";" -f1 test.csv > test/windSpeed.txt
+cut -d";" -f2 test.csv > test/windDir.txt
+cut -d";" -f3 test.csv > test/rea1.txt
+cut -d";" -f4 test.csv > test/rea2.txt
+cut -d";" -f5 test.csv > test/rea3.txt
+cut -d";" -f6 test.csv > test/rea4.txt
+cut -d";" -f7 test.csv > test/rea5.txt
+cut -d";" -f8 test.csv > test/rea6.txt
+cut -d";" -f9 test.csv > test/rea7.txt
+cut -d";" -f10 test.csv > test/rea8.txt
+cut -d";" -f11 test.csv > test/rea9.txt
+cut -d";" -f12 test.csv > test/rea10.txt
+cut -d";" -f13 test.csv > test/rea11.txt
+cut -d";" -f14 test.csv > test/rea12.txt
+cut -d";" -f15 test.csv > test/ramp.txt
+
+cut -d";" -f1 train.csv > train/windSpeed.txt
+cut -d";" -f2 train.csv > train/windDir.txt
+cut -d";" -f3 train.csv > train/rea1.txt
+cut -d";" -f4 train.csv > train/rea2.txt
+cut -d";" -f5 train.csv > train/rea3.txt
+cut -d";" -f6 train.csv > train/rea4.txt
+cut -d";" -f7 train.csv > train/rea5.txt
+cut -d";" -f8 train.csv > train/rea6.txt
+cut -d";" -f9 train.csv > train/rea7.txt
+cut -d";" -f10 train.csv > train/rea8.txt
+cut -d";" -f11 train.csv > train/rea9.txt
+cut -d";" -f12 train.csv > train/rea10.txt
+cut -d";" -f13 train.csv > train/rea11.txt
+cut -d";" -f14 train.csv > train/rea12.txt
+cut -d";" -f15 train.csv > train/ramp.txt
